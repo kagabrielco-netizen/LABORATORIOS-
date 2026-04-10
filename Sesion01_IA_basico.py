7fcb2b07d6e1974e4e80a5dea51126c5175e1f56
kagabrielco-netizen <kagabrielco@ucvvirtual.edu.pe>
2026-04-09 19:05:57 -0500

    Add files via upload

---
+++ Sesion01_IA_basico.py
@@ -0,0 +1,29 @@
+print("Curso: Sistemas Inteligentes")
+print("Sesión 1 - Introducción a IA")
+# Ejemplo básico de operación matemática
+a = 20
+b = 24
+suma = a + b		
+print("La suma es:", suma)
+
+c = 3
+d = 5
+multilplicacion = c * d
+print("La multiplicaciòn es igual a: ", multilplicacion)
+
+f = 30
+e = 5
+division = f / e
+print("La divisiòn es:", division)
+
+# Ejemplo de estructura condicional
+if suma > 30 :
+        print("La suma es mayor que 30")
+else:
+        print("La suma es menor o igual que 30")
+        
+numero = int(input("Ingrese un número: "))
+print("El número ingresado es:", numero)
+
+
+        
